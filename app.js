9618c309e71b5fb75070ad553b8d9dac7195a196
Triana Cerda <trianaRcerda@gmail.com>
2021-06-25 14:46:29 -0700

    working verb input and btn

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-// import functions
+const verbInput = document.getElementById('verb');
+const verbSpan = document.getElementById('verb-id');
+const btn = document.getElementById('wapify-btn');
+const nounInput = document.getElementById('noun');
+const nounSpan = document.getElementById('noun-id');
+const noun2Input = document.getElementById('noun2');
+const noun2Span = document.getElementById('noun2-id');
+const noun3Input = document.getElementById('noun3');
+const noun3Span = document.getElementById('noun3-id');
+const noun4Input = document.getElementById('noun4');
+const noun4Span = document.getElementById('noun4-id');
+const pluralNounInput = document.getElementById('plural-noun');
+const pluralNounSpan = document.getElementById('pluralnoun-id');
+const adjectiveInput = document.getElementById('adjective');
+const adjectiveSpan = document.getElementById('adjective-id');
 
-// reference needed DOM elements
-
-
-// set event listeners 
-  // get user input(s)
-  // do any needed work with the value(s)
-  // update DOM to reflect new value(s)
+btn.addEventListener('click', ()=> {
+    const verb = (verbInput.value);
+    verbSpan.textContent = verbInput.value;
+});
